8dfea32bc11c68abff6a29f6b52819739631aea5
23262 <23262@students.mrgs.school.nz>
2026-05-01 12:31:05 +1200

    adding image as the background

--- assessment.py
+++ assessment.py
@@ -2,8 +2,12 @@ import tkinter
 from tkinter import PhotoImage
 root=tkinter.Tk()
 root.title("New Zealand History Quiz")
+#setting geometry
 root.geometry("1000x750")
-image_path=
-
+image_path=PhotoImage(=r"C:\Users\23262\OneDrive - Mt Roskill Grammar School\Pictures\python tkinter assessment\Intro image .png")
 
 root.mainloop()
+
+
+
+
