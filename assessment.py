1d633d895aecda422f846689ea19e2d7fb0b8df7
23262 <23262@students.mrgs.school.nz>
2026-05-06 14:28:39 +1200

    restarted layout of tkinter code and also adding image

--- assessment.py
+++ assessment.py
@@ -1,13 +1,18 @@
-import tkinter
-from tkinter import PhotoImage
-root=tkinter.Tk()
+import tkinter as tk
+
+root = tk.Tk
+
+#title of gui window
 root.title("New Zealand History Quiz")
-#setting geometry
-root.geometry("1000x750")
-image_path=PhotoImage(=r"C:\Users\23262\OneDrive - Mt Roskill Grammar School\Pictures\python tkinter assessment\Intro image .png")
+
+#setting geometry of gui window
+root.geometry("500x560")
+
+#background image of the homepage
+bg = tk.PhotoImage(file="Intro image .png")
+
+#creating label, holds widget inside the window
+label= tk.Label(image=bg)
+label.pack()
 
 root.mainloop()
-
-
-
-
