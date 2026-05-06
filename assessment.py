dc8cf7ad64b51d41c0f5183774b22d596963d089
23262 <23262@students.mrgs.school.nz>
2026-05-06 14:50:52 +1200

    image resizing with the use of pillow

--- assessment.py
+++ assessment.py
@@ -1,18 +1,31 @@
 import tkinter as tk
-
+from PIL import Image, ImageTk
 root = tk.Tk
 
 #title of gui window
 root.title("New Zealand History Quiz")
 
 #setting geometry of gui window
-root.geometry("500x560")
+root.geometry("2000x3000")
 
 #background image of the homepage
 bg = tk.PhotoImage(file="Intro image .png")
 
-#creating label, holds widget inside the window
+#creating label that holds widget inside the window
 label= tk.Label(image=bg)
+#label.pack displays that label inside the window
 label.pack()
 
+image = Image.open("Intro image .png")
+resized_image = image.resize((50,50))
+#converts the image into a format Tkinter can understand
+img = ImageTk.PhotoImage(resized_image)
+label = tk.Label(image=img)
+label.image = img
+label.pack()
+
+
+
+
+#root.mainloop keeps the window open for events to happen
 root.mainloop()
