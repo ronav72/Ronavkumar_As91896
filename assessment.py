e12d671c5e9f2efb832f4ecae70a50190389a6ba
23262 <23262@students.mrgs.school.nz>
2026-05-07 13:30:02 +1200

    sectioning code

--- assessment.py
+++ assessment.py
@@ -2,16 +2,22 @@ import tkinter as tk
 from PIL import Image, ImageTk
 root = tk.Tk()
 
+
+
+
+
+# Quiz Homepage
+
 #title of gui window
 root.title("New Zealand History Quiz")
 
 #setting geometry of gui window
-root.geometry("800x600")
+root.geometry("850x650")
 
 
 
 image = Image.open("Intro image .png")
-resized_image = image.resize((800,600))
+resized_image = image.resize((850,650))
 #converts the image into a format Tkinter can understand
 img = ImageTk.PhotoImage(resized_image)
 label = tk.Label(image=img)
@@ -19,6 +25,34 @@ label = tk.Label(image=img)
 label.image = img
 #label.pack displays that label inside the window
 label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
