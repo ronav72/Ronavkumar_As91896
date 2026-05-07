17765eed2e0ee526acbc2df91228cf753f49ce6d
23262 <23262@students.mrgs.school.nz>
2026-05-07 13:06:17 +1200

    fixed the image resizing problem

--- assessment.py
+++ assessment.py
@@ -1,27 +1,23 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-root = tk.Tk
+root = tk.Tk()
 
 #title of gui window
 root.title("New Zealand History Quiz")
 
 #setting geometry of gui window
-root.geometry("2000x3000")
+root.geometry("800x600")
 
-#background image of the homepage
-bg = tk.PhotoImage(file="Intro image .png")
 
-#creating label that holds widget inside the window
-label= tk.Label(image=bg)
-#label.pack displays that label inside the window
-label.pack()
 
 image = Image.open("Intro image .png")
-resized_image = image.resize((50,50))
+resized_image = image.resize((800,600))
 #converts the image into a format Tkinter can understand
 img = ImageTk.PhotoImage(resized_image)
 label = tk.Label(image=img)
+#creating label that holds widget inside the window
 label.image = img
+#label.pack displays that label inside the window
 label.pack()
 
 
